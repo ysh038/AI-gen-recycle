8af6d464a47292e9fb3a25e54f2ceb23427922e4
ysh038 <71204343+ysh038@users.noreply.github.com>
2025-10-22 16:07:35 +0900

    feat: 테스트 로그인 성공 화면 추가

--- frontend/src/routes/Home/Home.tsx
+++ frontend/src/routes/Home/Home.tsx
@@ -1,9 +1,13 @@
 import styles from './Home.module.css'
+import { useAuthStore } from '../../stores/shared/user'
 
 function Home() {
+    const user = useAuthStore((state) => state.user)
     return (
         <main className={styles.main}>
-            <h1>Home</h1>
+            <h1>테스트 로그인 성공</h1>
+            <p>사용자 정보: {user?.user_id}</p>
+            <p>이메일: {user?.email}</p>
         </main>
     )
 }
